6366ced91b1babb735b485b50efc574322ff85d0
AsanoYuto8345 <al23057@shibaura-it.ac.jp>
2026-05-08 23:12:57 +0900

    echo化

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,46 +1,27 @@
 package handlers
 
 import (
-	"io"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
-func TodoListHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "Get All Todos!")
-	} else {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
+func TodoListHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Get All Todos")
 }
 
-func GetTodoByIdHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodGet {
-		io.WriteString(w, "do hogehoge")
-	} else {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
+func GetTodoByIdHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "do hogehoge")
 }
 
-func CreateTodoHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPost {
-		io.WriteString(w, "create New Todo!")
-	} else {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
+func CreateTodoHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "create new todo!")
 }
 
-func PutTodoByIdHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodPut {
-		io.WriteString(w, "update Todo!")
-	} else {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
+func PutTodoByIdHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "update todo with ID:2")
 }
 
-func DeleteTodoByIdHandler(w http.ResponseWriter, req *http.Request) {
-	if req.Method == http.MethodDelete {
-		io.WriteString(w, "delete 3Todo!")
-	} else {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-	}
+func DeleteTodoByIdHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "delete todo with ID:3")
 }
--- main.go
+++ main.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	handlers "go-todo-app/handlers"
-	"log"
-	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	http.HandleFunc("/todos/list", handlers.TodoListHandler)
-	http.HandleFunc("/todos/1", handlers.GetTodoByIdHandler)
-	http.HandleFunc("/todos", handlers.CreateTodoHandler)
-	http.HandleFunc("/todos/2", handlers.PutTodoByIdHandler)
-	http.HandleFunc("/todos/3", handlers.DeleteTodoByIdHandler)
-	log.Println("server start at port 8080")
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	e := echo.New()
+
+	e.GET("/todos/list", handlers.TodoListHandler)
+	e.GET("/todos/1", handlers.GetTodoByIdHandler)
+	e.POST("/todos", handlers.CreateTodoHandler)
+	e.PUT("/todos/2", handlers.PutTodoByIdHandler)
+	e.DELETE("/todos/3", handlers.DeleteTodoByIdHandler)
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
