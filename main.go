c797d272135cb4dd95e9c76af75127dda17a01bc
AsanoYuto8345 <al23057@shibaura-it.ac.jp>
2026-05-14 10:38:11 +0900

    Marshal対応

--- main.go
+++ main.go
@@ -1,12 +1,39 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	handlers "go-todo-app/handlers"
+	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
+type Todo struct {
+	ID        int       `json:"id"`
+	Title     string    `json:"title"`
+	Content   string    `json:"content"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdateAt  time.Time `json:"updated_at"`
+}
+
 func main() {
+	todo1 := Todo{
+		ID:        1,
+		Title:     "todo1 Title",
+		Content:   "todo1 content",
+		CreatedAt: time.Now(),
+		UpdateAt:  time.Now(),
+	}
+
+	jsonData, err := json.Marshal(todo1)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(string(jsonData))
+
 	e := echo.New()
 
 	e.GET("/todos/list", handlers.TodoListHandler)
@@ -16,4 +43,5 @@ func main() {
 	e.DELETE("/todos/:id", handlers.DeleteTodoByIdHandler)
 
 	e.Logger.Fatal(e.Start(":8080"))
+
 }
